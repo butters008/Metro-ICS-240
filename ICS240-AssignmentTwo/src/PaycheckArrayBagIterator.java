129e790ccb4d20216779e782f6f375a5396ad1ff
Butters <k.butterfield08@gmail.com>
2020-08-09 11:32:39 -0500

    Nate to look at this

---
+++ ICS240-AssignmentTwo/src/PaycheckArrayBagIterator.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class PaycheckArrayBagIterator implements Iterator<Paycheck> {
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Paycheck next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
